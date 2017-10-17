97f31a6f9ddbf6076f6ff58ed55ac302bea535de
BigXin12 <chengxin12@outlook.com>
2017-10-17 19:34:08 +0800

    前端测试完成

---
+++ tt-manager/tt-manager-web/src/main/java/com/cx/ttshop/web/IndexAction.java
@@ -0,0 +1,15 @@
+package com.cx.ttshop.web;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Scope("prototype")
+public class IndexAction {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
